f6064f354ef95953acee2a7e1d15eb1f486393e2
greykor <greykor@bk.ru>
2019-02-27 00:24:50 +0300

    add parsing line

--- select_words.py
+++ select_words.py
@@ -1,8 +1,9 @@
 import csv
-
-with open("100IRverbs.csv", newline='') as f:
-    reader = csv.reader(f, delimiter=";")
-    for line in reader:
-        print(line[1])
-
-        test test
+n = 1
+p = []
+test = 'test1 test2 test3'
+with open("100IRverbs.csv") as file:
+    lines = file.readlines()
+lines = ' '.join(lines[n:n+1])
+p = lines.split(' ')
+print(p)
